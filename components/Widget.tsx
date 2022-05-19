60bebe3a0c0d0fd6698b2520c826eb97f6d2ee01
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-19 19:15:08 -0400

    add react-twitter-embed

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,15 +1,23 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 const Widget = () => {
   return (
-    <div>
-       <div className="flex items-center space-x-2 bg-gray-100 rounded-full p-3 mt-2">
-          <SearchIcon className="h-5 w-5" />
-          <input type="text" placeholder="Search Twitter" 
-           className="bg-transparent"
-          />
-        </div> 
+    <div className="mt-2 px-2">
+      <div className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 bg-transparent outline-none"
+        />
+      </div>
+      <TwitterTimelineEmbed 
+        sourceType="profile"
+        screenName='saurbhnemade'
+        options={{height: 400}}
+      />
     </div>
   )
 }
