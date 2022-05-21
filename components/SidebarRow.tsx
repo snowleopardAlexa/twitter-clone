7dec6a8b1556a8f342d966a2698fa38fdc17753a
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-21 18:50:51 -0400

    add icons

---
+++ components/SidebarRow.tsx
@@ -0,0 +1,17 @@
+import React, { SVGProps } from 'react'
+
+interface Props {
+    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+    title: string
+}
+
+const SidebarRow = ({Icon, title}: Props) => {
+  return (
+    <div>
+        <Icon />
+        <p>{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
